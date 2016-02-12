49099925964190a436b237292d7f280eafb0fa72
Sebastien Alaiwan <sebastien.alaiwan@allegrodvt.com>
2016-02-12 13:57:43 +0100

    More common stuff factorization

--- base.sh
+++ base.sh
@@ -12,3 +12,31 @@ unset CC
 unset CXX
 readonly MAKE="make -j`nproc` -k"
 export LDFLAGS=-s
+
+function extract_to
+{
+  local file=$1
+  local dir=$2
+
+	tar xlf "$file"
+	rm -rf "$dir"
+	mv "`tar tlf $file 2>/dev/null | head -n 1 | cut -f 1 -d '/'`" "$dir"
+}
+
+function lazy
+{
+  local readonly func="$1"
+
+  if [ -f $BIN/flags/$func ] ; then
+    return 0
+  fi
+
+  echo "$func ..."
+  $func & # fork: prevent the callee to change the caller's environment
+  pid=$!
+  wait $pid
+
+  mkdir -p $BIN/flags
+  touch $BIN/flags/$func
+}
+
